ac2951bff2030512c9300e3ba4b4fa483e01e46c
Barq-dps <83699147+Barq-dps@users.noreply.github.com>
2025-02-12 16:18:03 +0200

    Update WordSearchSolver.h

--- WordSearchSolver.h
+++ WordSearchSolver.h
@@ -1,4 +1,4 @@
-﻿#ifndef WORD_SEARCH_SOLVER_H
+#ifndef WORD_SEARCH_SOLVER_H
 #define WORD_SEARCH_SOLVER_H
 
 #include <vector>
@@ -12,12 +12,14 @@ private:
     // Places a word into the grid at a random valid position
     bool placeWordInGrid(const std::string& word);
 
+
 public:
     WordSearchSolver();
     void loadGrid(int size);
     void displayGrid();
     std::vector<std::string> solve();
     void saveGridToFile(const std::string& filename);
+    
     const std::vector<std::vector<char>>& getGrid() const {
         return grid;
     }
